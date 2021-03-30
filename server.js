71003302c077d68da5b7e3782192a85fa94342d2
matthew-io <xyzmonte@protonmail.com>
2021-03-30 11:26:23 +0100

    setting up to push to heroku

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 let devicesCollection,
   serverString =
     "mongodb+srv://user123:user123@cluster0.1a9nk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("listening on port 3000");
 });
 
@@ -21,7 +24,9 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-MongoClient.connect(serverString, { useUnifiedTopology: true })
+MongoClient.connect(process.env.MONGODB_URI || serverString, {
+  useUnifiedTopology: true,
+})
   .then((client) => {
     console.log("Connected to Database");
     const db = client.db("akamai-devices");
